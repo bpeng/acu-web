1f4638bb21749f82b81ecc27efde4eabb6e953fc
bpeng <b.peng@gns.cri.nz>
2021-07-08 15:55:55 +1200

    php update

--- pages/home.php
+++ pages/home.php
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-   include_once('header.php');
+   //echo "path is $path ";
+   //echo "<script>console.log('##2 home path: " . $path . "' );</script>";
+   include_once('contents/header.html');
 ?>
 
 <body>
   <div class="container">
   <div class="accontainer art-postcontent mt-3 mb-3">
   <?php
-    include_once('nav.php');
+    include_once('contents/nav.html');
   ?>
 
    <div class="row container">
       <!-- left pane -->
       <div class="col-md-9 col-sm-12">
          <?php
-            include_once('contents/acupuncture.html');
-         ?>
-         <!--- bottom menu-->
-         <?php
-           include_once('bottom_nav1.php');
+            include_once("contents/$path.html");
+         //<!--- bottom menu-->
+           include_once('contents/bottom_nav1.html');
          ?>
        </div>
        <!--- right pane -->
        <div class="col-md-3 col-sm-12">
          <?php
-           include_once('right_pane.php');
+           include_once('contents/right_pane.html');
          ?>
        </div>
    </div>
 
     <?php
-       include_once('footer.php');
+       include_once('contents/footer.html');
     ?>
     </div>
   </div>
 </body>
 </html>
-
--- pages/index.php
+++ pages/index.php
@@ -6,21 +6,44 @@ include 'src/Steampixel/Route.php';
 
 // Define a global basepath
 define('BASEPATH','/');
+define ("ALL_PATH", array ("acupuncture","home","white"));
+
+function checkPath($path) {
+    //$ALL_PATH = array("acupuncture","home","white");
+    return in_array($path, ALL_PATH, TRUE);
+}
+
+Route::add('/([a-z-0-9-]*)', function($path) {
+  if ($path == ""){//home page
+      $path = 'acupuncture';
+  }
+  if(checkPath($path) == TRUE) {
+     include('home.php');
+  } else {//invalid path
+    header('HTTP/1.0 404 Not Found');
+    echo 'Error 404 :-(<br>';
+    echo 'The requested path "'.$path.'" was not found!';
+  }
+});
+
+
 
 // Add base route (startpage)
+/****
+// Add base route (startpage)
 Route::add('/', function() {
-  include('home.php');
+     echo "<script>console.log('##0 path:' );</script>";
+     $path = 'acupuncture';
+     include('home.php');
 });
 
-Route::add('/test', function() {
-  //navi();
-  echo 'You are not really on index.php ;-)';
+// Add base route (startpage)
+Route::add('/acupuncture', function() {
+     $path = 'acupuncture';
+     echo "<script>console.log('##1 path: " . $path . "' );</script>";
+     include('home.php');
 });
-
-Route::add('/home', function() {
-  include('home.php');
-});
-
+****/
 // Run the Router with the given Basepath
 Route::run(BASEPATH);
 
